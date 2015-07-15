c35797c75d629a22a74ecfcf52127e92a34dc4d4
Stanislav Hannes <stani180@t-online.de>
2015-07-15 19:11:17 +0200

    kleines Update zur Abgabe

--- main/Main.java
+++ main/Main.java
@@ -17,125 +17,120 @@ import codegen.*;
 
 class Main {
 
-	static private String inFileName = null;
-	static private String outFileName = null;
-	static private boolean optionTokens = false;
-	static private boolean optionAbsyn = false;
-	static private boolean optionTables = false;
-	static private boolean optionVars = false;
+  static void help() {
+    /* show some help how to use the program */
+    System.out.print("Usage: spl [options] <input file> <output file>\n");
+    System.out.print("Options:\n");
+    System.out.print("  --tokens         show stream of tokens\n");
+    System.out.print("  --absyn          show abstract syntax\n");
+    System.out.print("  --tables         show symbol tables\n");
+    System.out.print("  --vars           show variable allocation\n");
+    System.out.print("  --help           show this help\n");
+  }
 
-	private static void errmsg(String msg){
-		System.err.println(msg);
-	}
+  public static void main(String[] args) {
+    String inFileName = null;
+    String outFileName = null;
+    boolean optionTokens = false;
+    boolean optionAbsyn = false;
+    boolean optionTables = false;
+    boolean optionVars = false;
+    for (int i = 0; i < args.length; i++) {
+      if (args[i].charAt(0) == '-') {
+        /* option */
+        if (args[i].equals("--tokens")) {
+          optionTokens = true;
+        } else
+        if (args[i].equals("--absyn")) {
+          optionAbsyn = true;
+        } else
+        if (args[i].equals("--tables")) {
+          optionTables = true;
+        } else
+        if (args[i].equals("--vars")) {
+          optionVars = true;
+        } else
+        if (args[i].equals("--help")) {
+          help();
+          System.exit(0);
+        } else {
+          System.out.println(
+            "**** Error: unrecognized option '" +
+            args[i] + "'; try 'spl --help'"
+          );
+          System.exit(1);
+        }
+      } else {
+        /* file */
+        if (outFileName != null) {
+          System.out.println(
+            "**** Error: more than two file names not allowed"
+          );
+          System.exit(1);
+        }
+        if (inFileName != null) {
+          outFileName = args[i];
+        } else {
+          inFileName = args[i];
+        }
+      }
+    }
+    if (inFileName == null) {
+      System.out.println("**** Error: no input file");
+      System.exit(1);
+    }
+    if (outFileName == null) {
+      System.out.println("**** Error: no output file");
+      System.exit(1);
+    }
+    try {
+      FileInputStream source = new FileInputStream(inFileName);
+      Scanner scanner = new Scanner(source);
+      if (optionTokens) {
+        Symbol token;
+        do {
+          token = scanner.next_token();
+          scanner.showToken(token);
+        } while (token.sym != sym.EOF);
+        System.exit(0);
+      }
+      Parser parser = new Parser(scanner);
+      DecList program = (DecList) parser.parse().value;
+      if (optionAbsyn) {
+        program.show(0);
+        System.out.println();
+        System.exit(0);
+      }
+      SemanticChecker semant = new SemanticChecker();
+      Table globalTable = semant.check(program, optionTables);
+      VarAllocator varalloc = new VarAllocator(globalTable, optionVars);
+      varalloc.allocVars(program);
+      try {
+        FileWriter outFile = new FileWriter(outFileName);
+        Codegenerator codegen = new Codegenerator(outFile);
+        codegen.genCode(program, globalTable);
+        outFile.close();
+      } catch (FileNotFoundException e) {
+        System.out.println("**** Error: cannot open output file '" +
+                outFileName + "'");
+        System.exit(1);
+      } catch (IOException e) {
+        System.out.println("**** Error: IO error on output file '" +
+                outFileName + "'");
+        System.exit(1);
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("**** Error: cannot open input file '" +
+              inFileName + "'");
+      System.exit(1);
+    } catch (IOException e) {
+      System.out.println("**** Error: IO error on input file '" +
+              inFileName + "'");
+      System.exit(1);
+    } catch (Exception e) {
+      System.out.println("**** Error: " + e.getMessage());
+      System.exit(1);
+    }
+  }
 
-	private static void help() {
-		/* show some help how to use the program */
-		errmsg("Usage: spl [options] <input file> <output file>");
-		errmsg("Options:");
-		errmsg("  --tokens         show stream of tokens");
-		errmsg("  --absyn          show abstract syntax");
-		errmsg("  --tables         show symbol tables");
-		errmsg("  --vars           show variable allocation");
-		errmsg("  --help           show this help");
-	}
-
-	private static void checkArguments(String[] args) {
-		for (String arg: args) {
-			if (arg.charAt(0) == '-') {
-				/* option */
-				if (arg.equals("--tokens"))
-					optionTokens = true;
-				else if (arg.equals("--absyn"))
-					optionAbsyn = true;
-				else if (arg.equals("--tables"))
-					optionTables = true;
-				else if (arg.equals("--vars"))
-					optionVars = true;
-				else if (arg.equals("--help")) {
-					help();
-					System.exit(0);
-				} 
-				else {
-					errmsg("**** Error: unrecognized option '" + arg + "'");
-					errmsg("try 'spl --help'");
-					System.exit(1);
-				}
-			} 
-			else {
-				/* filename */
-				if (outFileName != null) {
-					errmsg("**** Error: more than two file names not allowed");
-					System.exit(1);
-				}
-				if (inFileName != null)
-					outFileName = arg;
-				else
-					inFileName = arg;
-			}
-		}
-		
-		if (inFileName == null) {
-			errmsg("**** Error: no input file");
-			System.exit(1);
-		}
-		if (outFileName == null) {
-			errmsg("**** Error: no output file");
-			System.exit(1);
-		}
-	}
-
-	public static void main(String[] args) {
-
-		checkArguments(args);
-
-		try {
-			FileInputStream source = new FileInputStream(inFileName);
-			Scanner scanner = new Scanner(source);
-			if (optionTokens) {
-				Symbol token;
-				do {
-					token = scanner.next_token();
-					scanner.showToken(token);
-				} while (token.sym != sym.EOF);
-				System.exit(0);
-			}
-
-			DecList program = (DecList) new Parser(scanner).parse().value;
-
-			if (optionAbsyn) {
-				program.show(0);
-				System.out.println();
-				System.exit(0);
-			}
-
-			Table globalTable = new SemanticChecker().check(program, optionTables);
-			new VarAllocator(globalTable, optionVars).allocVars(program);
-			
-			try {
-				FileWriter outFile = new FileWriter(outFileName);
-				new Codegenerator(outFile).genCode(program, globalTable);
-				outFile.close();
-			} catch (FileNotFoundException e) {
-				errmsg("**** Error: cannot open output file '" +
-						outFileName + "'");
-				System.exit(1);
-			} catch (IOException e) {
-				errmsg("**** Error: IO error on output file '" +
-						outFileName + "'");
-				System.exit(1);
-			}
-		} catch (FileNotFoundException e) {
-			errmsg("**** Error: cannot open input file '" +
-					inFileName + "'");
-			System.exit(1);
-		} catch (IOException e) {
-			errmsg("**** Error: IO error on input file '" +
-					inFileName + "'");
-			System.exit(1);
-		} catch (Exception e) {
-			e.printStackTrace();
-			errmsg("**** Error: " + e.getMessage());
-			System.exit(1);
-		}
-	}
 }
